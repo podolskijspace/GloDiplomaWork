8d0d715bef08a6a5ea179f9d8dd3ff38530e43e7
Podolskij <Podolskij1994@inbox.ru>
2019-12-04 20:13:28 +0300

    Добавил табы

--- js/script.js
+++ js/script.js
@@ -248,4 +248,31 @@ document.addEventListener('DOMContentLoaded', () => {
         btnsPolitic.forEach(item => item.addEventListener('click', () => showModal('.popup-privacy')));
     };
     politicPopup();
+
+    const tabs = () => {
+        const   tabWrapper = document.querySelector('#scheme'),
+                    buttonList = tabWrapper.querySelector('.nav-list'),
+                    buttonsNav = buttonList.children,
+                    tabsText = tabWrapper.querySelectorAll('.scheme-description-block'),
+                    tabsImg = tabWrapper.querySelectorAll('.scheme-slider__slide'),
+                    imgWrapper = tabWrapper.querySelector('.scheme-slider');
+
+        let old = 0;
+        buttonList.addEventListener('click', event => {
+            const target = event.target;
+            if (target.matches('.scheme-nav__item')) {
+                buttonsNav[old].classList.remove('active');
+                tabsText[old].classList.remove('visible-content-block');
+                
+                old = target.dataset.number;
+                imgWrapper.insertAdjacentElement('afterbegin', tabsImg[old]);
+                buttonsNav[old].classList.add('active');
+                tabsText[old].classList.add('visible-content-block');
+
+                
+            }
+        })
+
+    };
+    tabs();
 });  
