82566ca47f40a7ef9d503a7c35187f711e5aeaed
Podolskij <Podolskij1994@inbox.ru>
2019-12-05 09:48:20 +0300

    Отправка формы

--- js/script.js
+++ js/script.js
@@ -279,4 +279,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
     };
     tabs();
+    const makeForm = (selector) => {
+        const successPopup = document.querySelector('.popup-thank');
+        let form = document.querySelector(selector),
+            statusMessage = document.createElement('div'),
+            inputs = form.querySelectorAll('input');
+
+        const outputData = (response) => {
+            if (response.status !== 200) {
+                throw new Error ('status network not 200');
+            }
+            showModal('.popup-thank');
+            inputs.forEach(item => item.value = '');
+        },
+        errorData = (error) => {
+            console.error(error);
+        };
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            const formData = new FormData(form);
+            let body = {};
+            for (let val of formData.entries()) {
+                body[val[0]] = val[1];
+            }
+            postData(body)
+            .then(outputData)
+            .catch(errorData);
+        });
+        const postData = (body) => { 
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
+        };
+        successPopup.querySelector('.close').addEventListener('click', () => unShowModal('.popup-thank'));
+    };
+    for (let i = 1; i <= 6; i++) {
+        makeForm(`#feedback${i}`);
+    }
 });  
