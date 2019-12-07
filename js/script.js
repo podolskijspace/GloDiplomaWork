7efe2c9cdb39fa2777a66c8b0a6889df0f3a0a59
Podolskij <Podolskij1994@inbox.ru>
2019-12-07 11:51:11 +0300

    Починил табы

--- js/script.js
+++ js/script.js
@@ -731,6 +731,54 @@ document.addEventListener('DOMContentLoaded', () => {
     //Слайдер с отзывами
     slider('.reviews-slider__slide', '.reviews-slider', '#reviews-arrow_left', '#reviews-arrow_right');
 
+    //Адаптивность переключателей 
+    const getResponseTabs = (wrapper, left, right) => {
+        let    width = wrapper.clientWidth,
+                elems = [...wrapper.children],
+                elemWidth = elems.reduce((accum, item) => {
+                    return accum + item.clientWidth;
+                }, 0) / elems.length,
+                numberElems = elems.length,
+                numberSpaces = numberElems + 1,
+                spaceWidth = (width -  numberElems * elemWidth) / numberSpaces,
+                position = spaceWidth + elemWidth,
+                visibleWidth = wrapper.parentElement.clientWidth,
+                steps = [],
+                currentPosition = 1,
+                arrowLeft = document.querySelector(left),
+                arrowRight = document.querySelector(right);
+        steps[0] = visibleWidth;
+        while (position <= width) {
+            if (position > visibleWidth) {
+                steps[currentPosition] = position; 
+                currentPosition++;
+            }
+            position += spaceWidth + elemWidth;
+        }
+        currentPosition = 0;
+        
+
+        arrowRight.addEventListener('click', () => {
+            currentPosition++;
+            if (currentPosition >= steps.length - 1) {
+                currentPosition = steps.length - 1;
+            }
+            let translateX = steps[currentPosition] - visibleWidth;
+            wrapper.style.transform = `translateX(-${translateX}px)`;
+        });
+
+        arrowLeft.addEventListener('click', () => {
+            currentPosition--;
+            if (currentPosition < 0) {
+                currentPosition = 0;
+            }
+            let translateX = steps[currentPosition] - visibleWidth;
+            wrapper.style.transform = `translateX(-${translateX}px)`;
+        });
+    };
+
+    getResponseTabs(document.getElementById('scheme-list'), '#nav-arrow-scheme_left', '#nav-arrow-scheme_right')
+
     //Блок виды ремонда. Слайдер и табы
 
     const moveRepairBlock = () => {
@@ -819,49 +867,6 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         };
         moveSliders();
-
-        const getResponseTabs = (wrapper, left, right) => {
-            let    width = wrapper.clientWidth,
-                    elems = wrapper.children,
-                    elemWidth = elems[0].clientWidth,
-                    numberElems = elems.length,
-                    numberSpaces = numberElems + 1,
-                    spaceWidth = (width -  numberElems * elemWidth) / numberSpaces,
-                    position = spaceWidth + elemWidth,
-                    visibleWidth = wrapper.parentElement.clientWidth,
-                    steps = [],
-                    currentPosition = 1,
-                    arrowLeft = document.querySelector(left),
-                    arrowRight = document.querySelector(right);
-            steps[0] = visibleWidth;
-            while (position <= width) {
-                if (position > visibleWidth) {
-                    steps[currentPosition] = position; 
-                    currentPosition++;
-                }
-                position += spaceWidth + elemWidth;
-            }
-            currentPosition = 0;
-            
-
-            arrowRight.addEventListener('click', () => {
-                currentPosition++;
-                if (currentPosition >= steps.length - 1) {
-                    currentPosition = steps.length - 1;
-                }
-                let translateX = steps[currentPosition] - visibleWidth;
-                tabsNav.style.transform = `translateX(-${translateX}px)`;
-            });
-
-            arrowLeft.addEventListener('click', () => {
-                currentPosition--;
-                if (currentPosition < 0) {
-                    currentPosition = 0;
-                }
-                let translateX = steps[currentPosition] - visibleWidth;
-                tabsNav.style.transform = `translateX(-${translateX}px)`;
-            });
-        };
 
         if (document.documentElement.clientWidth <= 1024) {
             getResponseTabs(tabsNav, '#nav-arrow-repair-left_base', '#nav-arrow-repair-right_base');
